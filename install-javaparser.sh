acf5829df5be3515f6191e557999235aaa5026d1
Owolabi Legunsen <legunsen@cornell.edu>
2022-02-11 11:28:17 -0500

    CI too hard to set up now

--- install-javaparser.sh
+++ install-javaparser.sh
@@ -3,7 +3,7 @@
     git clone https://github.com/javaparser/javaparser.git
     (
         cd javaparser
-        git co javaparser-parent-3.23.1
+        git checkout javaparser-parent-3.23.1
         sed -i 's/public final int hashCode/public int hashCode/' javaparser-core/src/main/java/com/github/javaparser/ast/Node.java
         mvn install -DskipTests -DskipITs
     )
