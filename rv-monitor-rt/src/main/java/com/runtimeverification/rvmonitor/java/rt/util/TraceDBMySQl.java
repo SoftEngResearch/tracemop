f88b02734ff90ab7b74a7b64339589698352d957
Owolabi Legunsen <legunsen@cornell.edu>
2023-05-16 16:44:22 -0400

    started adding a variant for MySQL

---
+++ rv-monitor-rt/src/main/java/com/runtimeverification/rvmonitor/java/rt/util/TraceDBMySQl.java
@@ -0,0 +1,186 @@
+package com.runtimeverification.rvmonitor.java.rt.util;
+
+import org.h2.tools.Csv;
+
+import java.io.File;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.sql.DriverManager.getConnection;
+
+public class TraceDBMySQl implements TraceDB{
+
+    private Connection connection;
+
+    private String jdbcURL = "jdbc:mysql://localhost:3306/mop?serverTimezone=UTC";
+    private String jdbcUsername = "mop";
+
+    private String jdbcPassword = "javamop";
+
+    private String tableName;
+
+    public TraceDBMySQl() {
+        this("defaultTableName");
+    }
+
+    public TraceDBMySQl(String tableName) {
+        this.connection = getConnection();
+        this.tableName  = tableName;
+    }
+
+    public Connection getConnection() {
+        if (connection != null) {
+            return connection;
+        }
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            printSQLException(e);
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+        return connection;
+    }
+
+    protected void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void put(String monitorID, String trace, int length) {
+        final String INSERT_TRACE_SQL = "INSERT INTO traces (monitorID, trace, length ) VALUES (?, ?, ?);";
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(INSERT_TRACE_SQL)) {
+            preparedStatement.setString(1, monitorID);
+            preparedStatement.setString(2, trace);
+            preparedStatement.setInt(3, length);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    @Override
+    public void update(String monitorID, String trace, int length) {
+        final String UPDATE_TRACE_SQL = "update traces set trace = ?, length = ? where monitorID = ?;";
+        try(PreparedStatement preparedStatement = getConnection().prepareStatement(UPDATE_TRACE_SQL)){
+            preparedStatement.setString(1, trace);
+            preparedStatement.setInt(2, length);
+            preparedStatement.setString(3, monitorID);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void createTable() {
+        final String createTableSQL = "create table traces (monitorID  varchar(400) not null , trace LONGTEXT, length int, primary key (monitorID));";
+        try (Statement statement = getConnection().createStatement()) {
+            statement.execute(createTableSQL);
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    @Override
+    public int uniqueTraces() {
+        String query = "select count(distinct(trace)) from traces";
+        int count = -1;
+        try (Statement statement = getConnection().createStatement()) {
+            ResultSet rs = statement.executeQuery(query);
+            if (rs.next()) {
+                count = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return count;
+    }
+
+    @Override
+    public int size() {
+        String query = "select count(*) from traces";
+        int count = -1;
+        try(Statement statement =  getConnection().createStatement()){
+            ResultSet rs = statement.executeQuery(query);
+            if (rs.next()) {
+                count = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return count;
+    }
+
+    @Override
+    public List<Integer> getTraceLengths() {
+        String query = "select length from traces";
+        List<Integer> lengths =  new ArrayList<>();
+        try (Statement statement = getConnection().createStatement()) {
+            ResultSet rs =  statement.executeQuery(query);
+            while (rs.next()) {
+                lengths.add(rs.getInt(1));
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return lengths;
+    }
+
+    @Override
+    public Map<String, Integer> getTraceFrequencies() {
+        String query = "select count(*), trace from traces group by trace";
+        Map<String, Integer> traceFrequency = new HashMap<>();
+        try(Statement statement = getConnection().createStatement()) {
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                traceFrequency.put(rs.getString(2), rs.getInt(1));
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return traceFrequency;
+    }
+
+    @Override
+    public void dump() {
+        String tableName = "traces";
+        final String SELECT_QUERY = "select * from traces into outfile '/var/lib/mysql-files/traaaces2.csv' fields terminated by ',';";
+        try(PreparedStatement preparedStatement = getConnection().prepareStatement(SELECT_QUERY)){
+            ResultSet rs = preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        TraceDBMySQl traceDB = new TraceDBMySQl("dummy");
+//        traceDB.createTable();
+//        traceDB.put("Spec1#4", "[a,b,b,c]", 4);
+//        traceDB.put("Spec1#5", "[a,b,b,c,c,c,c]", 7);
+
+
+//        traceDB.update("Spec1#1", "[a,b,b,c,d]", 5);
+        System.out.println("UNIQ: " + traceDB.uniqueTraces());
+        System.out.println("size: " + traceDB.size());
+        System.out.println("TL: " + traceDB.getTraceLengths());
+        System.out.println("TF: " + traceDB.getTraceFrequencies());
+        traceDB.dump();
+    }
+}
